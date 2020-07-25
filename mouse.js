ddc7c244397f4814bf506bc2a07519fcbc6e836a
PabloDurkheim <67520216+PabloDurkheim@users.noreply.github.com>
2020-07-25 15:49:35 -0300

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -17,6 +17,44 @@ function mouseClicked() {
 
   }
   
+  if(tela == 5 || tela == 6){
+
+  	if(mouseX >= 255 && mouseX <= 355 && mouseY >= 490 && mouseY <= 580)
+
+  		vida = 3;
+  		nivel = 1;
+  		vezes = 10;
+
+  		subt1 = parseInt(Math.random(0, 10)*vezes);
+		subt2 = parseInt(Math.random(0, 10)*vezes);	
+
+		gerarValores();
+
+		subtracaoPositivaOuNegativa();
+ 
+		escolheOndeFicaOResultado();
+
+		temp1 = 10;
+		temp2 = 15;
+		temp3 = 20;
+		temp4 = 25;
+		temp5 = 30;
+		temp6 = 35;
+		temp7 = 40;
+		temp8 = 45;
+		temp9 = 50;
+		temp10 = 60;
+
+  		audio5.stop();
+  		audio6.stop();
+  		audio2.loop();
+
+  		tela = 0;
+
+  		frameRate(600);
+
+  }
+
  
 }
 
